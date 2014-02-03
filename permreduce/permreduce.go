2ef57a530279a6943f64ca76d376e64d240b541f
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2014-02-02 21:32:16 -0800

    affirm that we are that user before running

--- permreduce/permreduce.go
+++ permreduce/permreduce.go
@@ -14,10 +14,22 @@ func main() {
     err := syscall.Setgid(*gid)
     if err != nil {
         log.Fatalf("Failed to switch to group id %d: %v\n", *gid, err)
+        os.Exit(1)
     }
     err = syscall.Setuid(*uid)
     if err != nil {
         log.Fatalf("Failed to switch to user id %d: %v\n", *uid, err)
+        os.Exit(1)
+    }
+    actual_uid := os.Getuid() 
+    if actual_uid != *uid {
+        log.Fatalf("Failed to switch to user id %d. Instead am %d\n", *uid, actual_uid)
+        os.Exit(1)
+    }
+    actual_gid := os.Getgid()
+    if actual_gid != *gid {
+        log.Fatalf("Failed to switch to group id %d. Instead am %d\n", *gid, actual_gid)
+        os.Exit(1)
     }
     args := flag.Args()
     if len(args) != 0 {
